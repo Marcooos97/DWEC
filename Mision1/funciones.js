f608ad4e97c4a8d6e467349d17cd478c163d5928
Marcooos97 <marcosalonso1115@gmail.com>
2020-10-06 12:14:06 +0200

    mision1

---
+++ Mision1/funciones.js
@@ -0,0 +1,36 @@
+function areaTriangulo(b, a){
+    return "El area del triangulo es: " + ((b*a)/2);
+}
+
+function perimetroTrianguloEquilatero(l){
+    return "El perimetro del Triangulo Equilatero es: " + (3*l);
+}
+
+function perimetroTrianguloIsosceles(l1,l2){
+    return "El perimetro del Triangulo Isosceles es: " + (2*l1 + l2);
+}
+
+function perimetroTrianguloEscaleno(l1,l2,l3){
+    return "El perimetro del Triangulo Escaleno es: " + (l1+l2+l3);    
+}
+
+function areaCuadrado(l){
+    return "El area del cuadrado es: " + (l*l);    
+}
+
+function perimetroCuadrado(l){
+    return "El perimetro del cuadrado es: " + (4*l);    
+}
+
+function areaRectangulo(l1,l2){
+    return "El area del rectangulo es: " + (l1*l2);    
+}
+
+function areaCirculo(r){
+    return "El area del circulo es: " + (Math.PI * (r*r));    
+}
+
+function longitudCircunferencia(r){
+    return "La longitud de la circuenferencia es: " + (2 * Math.PI * r);    
+}
+
---
+++ Mision1/variables.js
@@ -0,0 +1,7 @@
+var b=4;
+var a=3;
+var l=4;
+var l1=4;
+var l2=4;
+var l3=4;
+var r=4;
